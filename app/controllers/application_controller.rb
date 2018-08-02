fd8d008311b73a876873e23d17a8a6f7f6d74154
monet-jones <monet.s.jones@gmail.com>
2018-08-02 18:36:24 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,25 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :index
+  end
 
-end
+post '/life' do
+  @genre = params[:genre]
+  @time = params [:time]
+  @diet = params [:diet]
+  erb :life
+
+end 
+
+  
+ end 
+
+
+
+
+  
+  
+
