6ad9f7883bf03ca42c7abd8092a338496049e047
monet-jones <monet.s.jones@gmail.com>
2018-08-03 16:00:27 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,18 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  get '/life' do
+    erb :life
+  end
 
 post '/life' do
   @genre = params[:genre]
-  @time = params [:time]
-  @diet = params [:diet]
+  @time = params[:time]
+  @diet = params[:diet]
+  @workout= params[:workout]
+  @quote= params[:quote]
+  @women= params[:women]
   erb :life
 
 end 
